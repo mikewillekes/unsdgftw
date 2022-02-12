bab6dafa87ec283a087dc4dd60e0237dba535e80
Mike Willekes <mikewillekes@gmail.com>
2022-02-12 12:20:26 -0500

    Document and Paragraph metadata classes

---
+++ pipeline/config.py
@@ -0,0 +1,14 @@
+
+# Paths of the raw and processed documents
+CORPUS_DIR = 'corpus'
+RAW_DIR = '1-raw'
+TEXT_DIR = '2-text'
+CLEANTEXT_DIR = '3-cleantext'
+NLP_DIR = '4-nlp'
+
+# File extensions
+# RAW_EXTENSION is excluded b/c it can be anything that TIKA can support parsing
+TEXT_EXTENSION = '.xml'
+CLEANTEXT_EXTENSION = '.jsonl'
+
+
---
+++ pipeline/paragraph_metadata.py
@@ -0,0 +1,41 @@
+from datetime import date
+from serde import serialize, deserialize
+from serde.json import from_json, to_json
+from dataclasses import dataclass
+
+
+@deserialize
+@serialize
+@dataclass
+class ParagraphMetadata:
+    organization: str
+    local_filename: str
+    about_url: str
+    download_url: str
+    title: str
+    summary: str
+    year: date
+    page_number: int
+    paragraph_number: int
+    paragraph_len: int
+    keywords: List[str]
+    clean_text: str
+    raw_text: str
+
+
+def load_metadata(filename):
+    # Deserialized from Jsonl file
+    metadata = []
+    with (filename, 'r') as fp:
+        for line in fp.readlines():
+            metadata.append(from_json(ParagraphMetadata, line.strip()))
+    return metadata
+
+
+def save_metadata(filename, metadata):
+    # Serialize to json string & append newline
+    lines = []
+    for m in metadata:
+        lines.append(to_json(m) + '\n')
+    # Save to Jsonl file
+    open(filename, 'w').writelines(lines)
